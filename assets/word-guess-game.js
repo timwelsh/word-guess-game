a07d934e8472d6a85c03178301eda31f9194dd0a
Worthy <tim@timwelsh.com>
2018-12-17 21:08:58 -0500

    added more logic

--- assets/word-guess-game.js
+++ assets/word-guess-game.js
@@ -11,6 +11,8 @@ var guessesAllowed = 10;
 var displayArray = []; //Two dimensional array to keep current answer along with the empty spaces representing the unguessed word(s)
 var unguessedAnswer = [];
 var guessedLetters = [];
+var gameWon = false;
+var underscoreCounter = 0;
 
 document.addEventListener('keypress', guess);
 //document.getElementById('guesses').innerHTML = guessesAllowed;
@@ -39,15 +41,14 @@ for(var i = 0; i < bandNameArray.length; i++) { //Build displayArray to keep two
     displayArray.push(combine);
   }
 }
-
+document.getElementById("answer").innerHTML = displayArray.join(" ");
 // for(var i = 0; i < displayArray.length; i++) {
 //   unguessedAnswer = unguessedAnswer + displayArray[i][1];
 // }
-
 function guess() {
   input = event.key;
   if(guessedLetters.includes(input)) {
-    alert("You have already guessed this letter! ")
+    alert("You have already guessed this letter! ");
   }
   if(bandNameArray.includes(input)) {
     //document.getElementById("guessed").innerHTML = guessedLetters;
@@ -56,24 +57,34 @@ function guess() {
       if (input == bandNameArray[i]) {
         displayArray[i] = input;
         screenDisplay = displayArray;
-       //document.getElementById("answer").innerHTML = screenDisplay;
-       document.getElementById("answer").innerHTML = screenDisplay.join(" ")
+       document.getElementById("answer").innerHTML = screenDisplay.join(" ");
+      }
+    }
+    var underscoreCounter = 0;
+    for (var i = 0; i < screenDisplay.length; i++) {
+      if(screenDisplay[i] === "_") {
+        underscoreCounter = underscoreCounter + 1;
+      }
+      if (underscoreCounter === 0) {
+        gameWon = true;
       }
     }
     guessedLetters.push(input);
     document.getElementById("guessed").innerHTML = guessedLetters;
   } else {
-    if(guessedLetters.includes(input)) {
       guessedLetters.push(input);
       document.getElementById("guessed").innerHTML = guessedLetters;
-    } else {
-      //guessedLetters.push(input);
-      //console.log(guessedLetters + " these are guessed letters ");
     }
   }
+  //console.log(gameWon + " game won?");
+  //console.log(bandNameArray + " this is band name array ");
+  
+  if (gameWon) {
+    win = win + 1;
+    document.getElementById("wins").innerHTML = wins;
+    alert("congrats you won!");
+  }
+
+
 }
-
-
 //document.getElementById("displayArray").innerHTML = displayArray.join(" ");
-
-}
